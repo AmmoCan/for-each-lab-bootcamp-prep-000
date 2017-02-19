951f5db509a93db463a85af80dc6faf23cda0f0c
Ammo Can <ammo@2-drops.com>
2017-02-19 19:39:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  });
+}
+
+function iterate(callback) {
+  var array = ["Hello!", "Hi!", "Howdy!", "Hola!"]
+
+  array.forEach(callback);
+
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
